728e1ae890f6bc88f0a6d2c980e597c7d3a048a5
jcs9971 <112672563+jcs9971@users.noreply.github.com>
2023-02-03 15:39:12 -0500

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -21,6 +21,8 @@ def welcome_assignment_answers(question):
         answer = "No"
     elif question == "What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
         answer = 5
+    elif question == "What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
+        answer = 3
     else: 
         ### you should understand why this else case should be included
         ### what happens if there is a typo in one of the questions?
@@ -42,6 +44,6 @@ if __name__ == "__main__":
 #DONE "Is it possible to decode a message without a key? - Yes/No":
 #DONE "Is a hashed message supposed to be un-hashed? - Yes/No":
 #DONE "What is the SHA256 hashing value to the following message: 'NYU Computer Networking' - Use SHA256 hash generator and use the answer in your code":
-#DONE77 "Is MD5 a secured hashing algorithm? - Yes/No":
-#"What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
+#DONE "Is MD5 a secured hashing algorithm? - Yes/No":
+#DONE88"What layer of the TCP/IP model does the protocol DNS belong to? - The answer should be an integer number":
 #"What layer of the TCP/IP model does the protocol ICMP belong to? - The answer should be an integer number":
