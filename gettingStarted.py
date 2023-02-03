df74cedd2659080916cab3e73f8df11fcf0fbee4
jcs9971 <112672563+jcs9971@users.noreply.github.com>
2023-02-03 15:09:40 -0500

    Update gettingStarted.py

--- gettingStarted.py
+++ gettingStarted.py
@@ -8,7 +8,7 @@ def welcome_assignment_answers(question):
     if question == "Are encoding and encryption the same? - Yes/No":
         answer = "No"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "No"
+        answer = "Yes"
     else: 
         ### you should understand why this else case should be included
         ### what happens if there is a typo in one of the questions?
